d074fd6676fa641736b1df4698a7fadc6f6e2801
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-14 15:26:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,12 +80,8 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-        programmer_hash[:yukihiro_matsumoto] = {
-          :known_for => "Ruby", :languages => [
-            "LISP", "C"
-            ]
         }
+ 		} 
  		 
      
 
